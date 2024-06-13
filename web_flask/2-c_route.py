d189befce46c782a052ca53dc658a4327e8e149c
preshbridge <b.ogbuagu@alustudent.com>
2024-06-13 13:51:20 +0100

    web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,3 +22,4 @@ def c_is_fun(text):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,3 +31,4 @@ def python_is_cool(text='is cool'):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
